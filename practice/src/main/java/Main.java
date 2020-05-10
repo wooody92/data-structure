96fbf5b453e84369782980d177d5cec6d2523c7e
wooody92 <pkower4@naver.com>
2020-05-10 17:28:12 +0900

    [practice] 가우스 합을 구하는 함수
    가우스 공식을 이용한 합
    두 정수 사이의 합

--- practice/src/main/java/Main.java
+++ practice/src/main/java/Main.java
@@ -4,6 +4,6 @@ import chapter1.Chapter1_2;
 public class Main {
     public static void main(String[] args) {
         Chapter1_2 chapter1 = new Chapter1_2();
-        chapter1.sumWhile();
+        chapter1.sumGauss();
     }
 }
--- practice/src/main/java/chapter1/Chapter1_2.java
+++ practice/src/main/java/chapter1/Chapter1_2.java
@@ -3,8 +3,9 @@ package chapter1;
 import java.util.Scanner;
 
 public class Chapter1_2 {
+    Scanner str = new Scanner(System.in);
+
     public void sumWhile() {
-        Scanner str = new Scanner(System.in);
 
         System.out.println("n : ");
         int n = str.nextInt();
@@ -17,5 +18,54 @@ public class Chapter1_2 {
             i++;
         }
         System.out.println("result : " + sum);
+        System.out.println("i : " + i);
+    }
+
+    public void sumFor() {
+        System.out.println("n : ");
+        int n = str.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("result : " + sum);
+    }
+
+    public void sumGauss() {
+        System.out.println("n : ");
+        int n = str.nextInt();
+
+        int sum = 0;
+        int init = 1;
+        int additional = (n % 2) == 1? (n + 1) / 2 : 0;
+
+        sum = (init + n) * (n/2) + additional;
+        System.out.println("result : " + sum);
+    }
+
+    public void sumAll(int a, int b) {
+        int result = 0;
+
+        if (a < b) {
+            for (int i = a; i <= b; i++) {
+                result += i;
+            }
+            System.out.println("result : " + result);
+            return;
+        }
+        if (a > b) {
+            for (int i = b; i <= a; i++) {
+                result += i;
+            }
+            System.out.println("result : " + result);
+            return;
+        }
+        if (a == b) {
+            result = a;
+            System.out.println("result : " + result);
+            return;
+        }
     }
 }
